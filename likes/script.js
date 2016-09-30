6681bc7186a9730fd043e9d198bce6b1ba64700c
KatMacAttack <katmacattack@gmail.com>
2016-09-29 18:04:46 -0700

    Trying to get the .js to work

--- likes/script.js
+++ likes/script.js
@@ -1,24 +1,22 @@
 
 
-//counter function
-var i = 0;
+//counter function here:
+var like-link = document.querySelector (".like-link");
   function plusOneLike(event) {
   event.preventDefault();
   document.querySelector(".like-link").textContent = ++i;
 }
-
 //here's the plus one
 var a = document.querySelector("a");
 a.addEventListener("click", plusOneLike);
-
+//here's the new comment log
 function newComment (event) {
   console.log(event)
   event.preventDefault();
   var comment = document.createElement("div");
   comment.setAttribute("class", "comment");
   comment.textContent = document.querySelector("textarea").value;
-
-  var c = document.querySelector(".like");
+  var c = document.querySelector(".like-link");
   c.appendChild(comment);
   //clear the textarea after new comment is posted
   document.querySelector("textarea").value = "";
