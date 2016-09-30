5482b7c9c4cabc1f61ef4cde8d7cbf209fdec078
KatMacAttack <katmacattack@gmail.com>
2016-09-29 17:53:03 -0700

    chchchchanges

---
+++ likes/script.js
@@ -0,0 +1,27 @@
+<script>
+
+//counter function
+var i = 0;
+  function plusOneLike(event) {
+  event.preventDefault();
+  document.querySelector(".like-link").textContent = ++i;
+}
+
+//here's the plus one
+var a = document.querySelector("a");
+a.addEventListener("click", plusOneLike);
+
+function newComment (event) {
+  event.preventDefault();
+  var comment = document.createElement("new-comment-body");
+  comment.setAttribute("class", "comment");
+  comment.textContent = document.querySelector("textarea").value;
+  var c = document.querySelector(".like");
+  c.appendChild(comment);
+  //clear the textarea after new comment is posted
+  document.querySelector("textarea").value = "";
+}
+
+var b = document.querySelector("button");
+b.addEventListener("click", newComment);
+</script>
