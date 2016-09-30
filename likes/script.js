a253c5e855b6251a791409e6636aa5d8ad5514a3
KatMacAttack <katmacattack@gmail.com>
2016-09-29 18:19:36 -0700

    the js file won't link so adding into html for now

--- likes/script.js
+++ likes/script.js
@@ -1,26 +1,26 @@
-
-
+<script>
 //counter function here:
-var like-link = document.querySelector (".like-link");
+var likes = 0
   function plusOneLike(event) {
   event.preventDefault();
-  document.querySelector(".like-link").textContent = ++i;
+  document.querySelector(".like-count").newlike+1;
+  console.log(event);
 }
-//here's the plus one
+//here's the plus one on a click
 var a = document.querySelector("a");
 a.addEventListener("click", plusOneLike);
-//here's the new comment log
+//here's the add a new comment log
 function newComment (event) {
-  console.log(event)
   event.preventDefault();
   var comment = document.createElement("div");
   comment.setAttribute("class", "comment");
   comment.textContent = document.querySelector("textarea").value;
-  var c = document.querySelector(".like-link");
+  var c = document.querySelector(".like");
   c.appendChild(comment);
-  //clear the textarea after new comment is posted
+  //clear that text area
   document.querySelector("textarea").value = "";
 }
-
+//Making sure it executes with a click
 var b = document.querySelector("button");
 b.addEventListener("click", newComment);
+</script>
